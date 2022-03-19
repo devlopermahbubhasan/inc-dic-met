a23428011ad0d5553e51bfd72f15496437d8d66c
mahbub hasan <mahbubhasan2001d7@gmail.com>
2022-03-19 19:42:31 +0600

    metarial ui icons

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { useState } from 'react';
+import './Inc.css';
+import Button from '@mui/material/Button';
+import AddIcon from '@mui/icons-material/Add';
+import DeleteIcon from '@mui/icons-material/Delete';
+import Tooltip from '@mui/material/Tooltip';
+const App = () => {
+    const [count, setCount] = useState(0);
+    const incr = () =>{
+        setCount( count + 1);
+    }
+const dicr = () =>{
+    if (count > 0) {
+        setCount( count - 1);
+    }
+    else{
+        alert("sorry , zero not reach")
+        setCount(0)
+    };
+    
 }
+    return (
+        <div>
+            <div className='background'>
+                <div className='center'>
+                    <br />
+                    <br />
+                    <div className='count'>
+                        <h1>{count}</h1>
+                    </div>
+                    <div className='btn-section'>
+                      <Tooltip title="Add">
+                          <Button className='btn-INC' onClick={incr}><AddIcon/></Button>
+                      </Tooltip>
+                      <Tooltip title="Delete">
+                          <Button className='btn-DiC' onClick={dicr}><DeleteIcon/></Button>
+                      </Tooltip>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
 
 export default App;
